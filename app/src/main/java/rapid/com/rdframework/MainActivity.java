81b29411c6f36e1b5da483445135b30446187162
YLAndSoft <347933430@qq.com>
2018-09-12 22:33:10 +0800

    Initial commit

--- app/src/main/java/rapid/com/rdframework/MainActivity.java
+++ app/src/main/java/rapid/com/rdframework/MainActivity.java
@@ -1,13 +1,91 @@
 package rapid.com.rdframework;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Context;
+import android.support.v4.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import base.BaseMainActivity;
+import rapid.com.rdframework.fragments.ModularFiveFragment;
+import rapid.com.rdframework.fragments.ModularFourFragmentn;
+import rapid.com.rdframework.fragments.ModularsTwoFragment;
+import rapid.com.rdframework.fragments.ModularThreeFragment;
+import rapid.com.rdframework.fragments.ModularOneFragment;
+
+public class MainActivity extends BaseMainActivity {
+    /**模块1*/
+    ModularsTwoFragment twoFragment;
+    /**模块2*/
+    ModularOneFragment oneFragment;
+    /**模块3*/
+    ModularThreeFragment threeFragment;
+    /**模块4*/
+    ModularFourFragmentn fourFragmentn;
+    /**模块5*/
+    ModularFiveFragment fiveFragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void initData(Context mContext) {
+        //最后执行，做你自己的逻辑处理
+    }
+
+    @Override
+    public void initParams() {
+        //此方法会在绑定布局之前执行
+        setSetActionBarColor(true, rapid.com.base.R.color.ts_0);
+    }
+
+    @Override
+    public int[] selectImags() {
+        int[] imgsHovers = {
+                R.mipmap.ic_home_actionbar_select0,
+                R.mipmap.ic_home_actionbar_select1,
+                R.mipmap.ic_home_actionbar_select2,
+                R.mipmap.ic_home_actionbar_select3,
+                R.mipmap.ic_home_actionbar_select4};
+        return imgsHovers;
+    }
+
+    @Override
+    public int[] unSelectImags() {
+        int[] imageNormals = {
+                R.mipmap.ic_home_actionbar0,
+                R.mipmap.ic_home_actionbar1,
+                R.mipmap.ic_home_actionbar2,
+                R.mipmap.ic_home_actionbar3,
+                R.mipmap.ic_home_actionbar4};
+        return imageNormals;
+    }
+
+    //设置要绑定的模块
+    @Override
+    public List<Fragment> setFragments() {
+        return getListFragments();//获取Fragment集合
+    }
+
+    private List<Fragment> getListFragments(){
+        List<Fragment> list = new ArrayList<>();
+        if (oneFragment == null) {
+            oneFragment = new ModularOneFragment();
+        }
+        if (twoFragment == null) {
+            twoFragment = new ModularsTwoFragment();
+        }
+        if (threeFragment == null) {
+            threeFragment = new ModularThreeFragment();
+        }
+        if (fourFragmentn == null) {
+            fourFragmentn = new ModularFourFragmentn();
+        }
+        if (fiveFragment == null) {
+            fiveFragment = new ModularFiveFragment();
+        }
+        list.add(oneFragment);
+        list.add(twoFragment);
+        list.add(threeFragment);
+        list.add(fourFragmentn);
+//        list.add(fiveFragment);
+        return list;
     }
 }
---
+++ base/src/main/java/base/BaseConfig.java
@@ -0,0 +1,60 @@
+package base;
+
+/**
+ * @author: FYL
+ * @time: 2018/9/3
+ * @email:347933430@qq.com
+ * @describe: 工程配置相关
+ */
+public class BaseConfig {
+    private BaseConfig() {
+        throw new AssertionError();
+    }
+
+    /**
+     * 日志级别：verbose
+     */
+    public static final int LEVEL_1=1;//正常输出级别verbose
+    /**
+     * 日志级别：debug
+     */
+    public static final int LEVEL_2=2;//debug输出日志级别
+    /**
+     * 日志级别：error
+     */
+    public static final int LEVEL_3=3;//错误级别error
+
+
+    public static boolean isBaiDuStatistics;
+    public static boolean isLog=true;
+
+    /**
+     * 是否开启百度统计
+     * @param baiDuStatistics
+     */
+    public static void setBaiDuStatistics(boolean baiDuStatistics) {
+        isBaiDuStatistics = baiDuStatistics;
+    }
+    /**
+     * 是输出打印日志
+     * @param log
+     */
+    public static void setLog(boolean log) {
+        isLog = log;
+    }
+
+    /**
+     * 通过code码区分事件类型
+     */
+    public static final class EventCode {
+        public static final int CODE_A = 11;
+        public static final int CODE_B = 22;
+        public static final int CODE_C = 33;
+        public static final int CODE_D = 44;
+        public static final int CODE_E = 55;
+        public static final int CODE_F = 66;
+        public static final int CODE_G = 77;
+    }
+
+
+}
